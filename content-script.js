f8cd36cb78addecff4f98e3558cc6360c5e74df0
mochizuki <mochizuki@basicinc.jp>
2022-04-21 13:05:59 +0900

    feat: do not remove attribute if user is in admin

--- content-script.js
+++ content-script.js
@@ -1,5 +1,12 @@
 // ferret One のクリックイベント計測のトリガー属性を削除
-const elements = document.querySelectorAll('[data-hu-event]');
-[...elements].forEach(node => {
-  node.removeAttribute('data-hu-event');
-});
+const removeDataHuEventAttr = () => {
+  const elements = document.querySelectorAll('[data-hu-event]');
+  [...elements].forEach(node => {
+    node.removeAttribute('data-hu-event');
+  });
+}
+
+// ferret One 管理画面以外では、data-hu-event 属性を削除
+if (location.hostname !== 'app.ferret-one.com') {
+  removeDataHuEventAttr();
+}
